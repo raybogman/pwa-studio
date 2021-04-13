1ed8366d3f7e0cff8a73df91d95bc9129344a06f
raybogman <bogman@bogman.info>
2021-04-13 19:37:18 +0200

    Remove duplicated directory

--- pwa/node_modules/@magento/peregrine/lib/Apollo/constants.js
+++
@@ -1,2 +0,0 @@
-// This is used here in peregrine and in the storefront.
-export const CACHE_PERSIST_PREFIX = 'apollo-cache-persist';
--- pwa/node_modules/@magento/venia-ui/lib/util/makeUrl.js
+++
@@ -1,4 +0,0 @@
-/**
- * @deprecated Moved to @magento/peregrine/lib/util/makeUrl
- */
-export { default } from '@magento/peregrine/lib/util/makeUrl';
