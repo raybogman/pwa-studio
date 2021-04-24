75ba6462b05dcad8d7483f14d29219e3105d6efa
raybogman <bogman@bogman.info>
2021-04-24 14:19:46 +0200

    update .env 29

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,1 @@
-module.exports = api => {
-    const config = {
-        presets: ['@magento/peregrine'],
-        exclude: [/packages\/babel\-preset\-peregrine\//]
-    };
-    if (api.env() === 'development') {
-        // Ignore everything with underscores except stories in dev mode
-        config.exclude.push(/\/__(tests?|mocks|fixtures|helpers|dist)__\//);
-    }
-    return config;
-};
+module.exports = { presets: ['@magento/peregrine'] };
